12b3df48c5a88d2012aadad19675bc0ce0526715
Yure4ik <sandalenko1985@gmail.com>
2025-10-01 01:49:48 +0300

    dz2

---
+++ src/components/VoteOptions/VoteOptions.tsx
@@ -0,0 +1,33 @@
+import css from "./VoteOptions.module.css";
+import type { VoteType } from "../../types/votes";
+
+interface VoteOptionsProps {
+  onVote: (type: VoteType) => void;
+  onReset: () => void;
+  canReset: boolean;
+}
+
+export default function VoteOptions({
+  onVote,
+  onReset,
+  canReset,
+}: VoteOptionsProps) {
+  return (
+    <div className={css.container}>
+      <button className={css.button} onClick={() => onVote("good")}>
+        Good
+      </button>
+      <button className={css.button} onClick={() => onVote("neutral")}>
+        Neutral
+      </button>
+      <button className={css.button} onClick={() => onVote("bad")}>
+        Bad
+      </button>
+      {canReset && (
+        <button className={`${css.button} ${css.reset}`} onClick={onReset}>
+          Reset
+        </button>
+      )}
+    </div>
+  );
+}
---
+++ src/types/votes.ts
@@ -0,0 +1,7 @@
+export type VoteType = 'good' | 'neutral' | 'bad';
+
+export interface Votes {
+  good: number;
+  neutral: number;
+  bad: number;
+}
