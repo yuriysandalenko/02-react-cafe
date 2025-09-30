cb5401fc8f1b08ffb49cf55f63cef0957ea82021
Yure4ik <sandalenko1985@gmail.com>
2025-10-01 02:12:13 +0300

    dz2.1

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './components/app/App.tsx'
+import App from './components/App/App.tsx'
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
